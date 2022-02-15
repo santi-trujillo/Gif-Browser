68299dd526dab08985e646f5a5a9cb0977179695
Santiago <santitrujillo1105@gmail.com>
2022-02-15 10:59:16 -0500

    pagination

--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -2,8 +2,11 @@ import React, {useEffect, useState} from 'react'
 import Gif from './Gif'
 import GetGifs from './GetGifs'
 
+const INITIAL_PAGE = 0
+
 export default function ListOfGifs ({ params }) {
     const { keyword } = params
+    const [page, setPage] = useState(INITIAL_PAGE)
     const [gifs, setGifs] = useState([])
 
 
@@ -14,16 +17,24 @@ export default function ListOfGifs ({ params }) {
             })
     }, [keyword])
 
+    useEffect(function () {
+        GetGifs({keyword, page})
+        .then(nextPage => {
+            setGifs(prevGifs => prevGifs.concat(nextPage))
+        })
+    }, [page])
+
     return <div className="gifBox">
         {
-            gifs.map(({id, title, url}) => 
+            gifs.map(({id, title, url}, index) => 
                 <Gif
-                    key={id}
+                    key={`${id}-${index}`}
                     id={id}
                     title={title}
                     url={url}
                 />
             )
         }
+        <button onClick={() => setPage(1)}>Next page</button>
     </div>
 }
